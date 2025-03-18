0e78758d719b9e21a34459c960f1462936414f02
laithethanh <laithanh@1006@gmail.com>
2025-03-18 14:35:47 +0700

    edit

--- admin.py
+++ admin.py
@@ -6,28 +6,28 @@ screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 root.title("ADMIN")
 root.geometry(f"{screen_width}x{screen_height}")
-root.configure(bg="lightgreen")
+root.configure(bg="white")
 
-maintheme_frame = Frame(root, bg="lightgreen")
+maintheme_frame = Frame(root, bg="white")
 maintheme_frame.pack(fill=BOTH, expand=False)
 maintheme_lable = Label(
     maintheme_frame,
     text="NHẬN DIỆN BIỂN SỐ VÀ BẮN TỐC ĐỘ",
     font=("Arial,", 40, "bold"),
-    bg="lightgreen",
+    bg="white",
     fg="red",
 )
 maintheme_lable.pack(pady=10)
 
-menu_frame = Frame(root, bg="orange")
+menu_frame = Frame(root, bg="#e0f7fa")
 menu_frame.pack(side=LEFT, fill=Y, anchor=NW, padx=0, pady=0)
 menu_lable = Label(
-    menu_frame, text="Menu", font=("Arial", 16, "bold"), bg="orange", fg="white"
+    menu_frame, text="Menu", font=("Arial", 16, "bold"), bg="#e0f7fa", fg="black"
 )
 menu_lable.pack(pady=10)
 
 original_image_TrangChu = Image.open(
-    r"C:\Users\lai the thanh\Desktop\PYTHON\DO_AN_PYTHON\IconMenu\homePage.png"
+    r"C:\Users\lai the thanh\Desktop\PYTHON\Quan-Li-Phuong-Tien-Giao-Thong\IconMenu\homePage.png"
 )
 resized_image_TrangChu = original_image_TrangChu.resize((30, 30))
 iconTrangChu = ImageTk.PhotoImage(resized_image_TrangChu)
@@ -35,7 +35,7 @@ menu_TrangChu = Button(
     menu_frame,
     text="Trang chủ",
     font=("Arial", 12, "bold"),
-    bg="orange",
+    bg="#e0f7fa",
     width=25,
     bd=0,
     anchor=W,
@@ -50,16 +50,16 @@ menu_TrangChu.pack(fill=X, pady=2)
 
 menu_TraCuu = Button(
     menu_frame,
-    text="Tra cứu",
+    text="Quản lí phương tiện",
     font=("Arial", 12, "bold"),
-    bg="orange",
+    bg="#e0f7fa",
     width=25,
     bd=0,
     anchor=W,
     padx=10,
     pady=5,
     borderwidth=5,
-    command=lambda: showContent("Tra cứu"),
+    command=lambda: showContent("Quản lí phương tiện"),
 )
 menu_TraCuu.pack(fill=X, pady=2)
 
@@ -67,7 +67,7 @@ menu_TaiKhoan = Button(
     menu_frame,
     text="Tài khoản",
     font=("Arial", 12, "bold"),
-    bg="orange",
+    bg="#e0f7fa",
     width=25,
     bd=0,
     anchor=W,
@@ -82,7 +82,7 @@ menu_DSViPham = Button(
     menu_frame,
     text="Danh sách vi phạm",
     font=("Arial", 12, "bold"),
-    bg="orange",
+    bg="#e0f7fa",
     width=25,
     bd=0,
     anchor=W,
@@ -93,12 +93,12 @@ menu_DSViPham = Button(
 )
 menu_DSViPham.pack(fill=X, pady=2)
 
-content_frame = Frame(root, bg="lightgreen")
+content_frame = Frame(root, bg="white")
 content_frame.pack(fill=BOTH, expand=True)
 title_name_menu_lable = Label(
     content_frame,
     text="Trang chủ",
-    bg="orange",
+    bg="#e0f7fa",
     font=("Arial", 20, "bold"),
     pady=8,
     bd=3,
@@ -111,12 +111,12 @@ def showContent(item):
     global content_frame
     if content_frame:
         content_frame.destroy()
-    content_frame = Frame(root, bg="lightgreen")
+    content_frame = Frame(root, bg="white")
     content_frame.pack(fill=BOTH, expand=True)
     title_name_menu_lable = Label(
         content_frame,
         text=item,
-        bg="orange",
+        bg="#e0f7fa",
         font=("Arial", 20, "bold"),
         pady=8,
         bd=3,
